99483b1eb4ee998fbbee8ffc9724736bd50b36ad
ruslanizz <69423556+ruslanizz@users.noreply.github.com>
2021-03-18 09:56:09 +0500

    add filter and sorting to admin

--- skuproject/products/admin.py
+++ skuproject/products/admin.py
@@ -9,18 +9,26 @@ class UploadedBaseInfoAdmin(admin.ModelAdmin):
 class SeasonAdmin(admin.ModelAdmin):
     list_display = ['season_firstletters', 'name', 'user']
     search_fields = ['season_firstletters']
+    list_filter = ['name', 'user']
+    ordering = ['season_firstletters', 'name', 'user']
 
 class CapsuleAdmin(admin.ModelAdmin):
     list_display = ['capsule_firstletters', 'name', 'user']
     search_fields = ['capsule_firstletters']
+    list_filter = ['user']
+    ordering = ['capsule_firstletters', 'name', 'user']
 
 class SKUAdmin(admin.ModelAdmin):
     list_display = ['sku_firstletters', 'name', 'user']
     search_fields = ['sku_firstletters']
+    list_filter = ['user']
+    ordering = ['sku_firstletters', 'name', 'user']
 
 class SizeAdmin(admin.ModelAdmin):
     list_display = ['sku_full', 'quantity_sold','quantity_instock','quantity_onway', 'user']
     search_fields = ['sku_full']
+    list_filter = ['user']
+    ordering = ['sku_full', 'quantity_sold','quantity_instock','quantity_onway', 'user']
 
 
 admin.site.register(UploadedBaseInfo, UploadedBaseInfoAdmin)
